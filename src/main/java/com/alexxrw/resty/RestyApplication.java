7879bc9e569b99553c829cd8aa4fc2cc626b3996
Alexander <d5enemy@gmail.com>
2019-01-07 12:29:00 +0200

    Initial commit

---
+++ src/main/java/com/alexxrw/resty/RestyApplication.java
@@ -0,0 +1,14 @@
+package com.alexxrw.resty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestyApplication.class, args);
+	}
+
+}
+
